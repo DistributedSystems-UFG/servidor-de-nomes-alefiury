dace718a2e2d25c3f2ddfcdd8d2d404cc51d8569
alefiury <alefiury14@gmail.com>
2022-04-06 17:40:48 -0300

    Update

--- name_server.py
+++ name_server.py
@@ -19,7 +19,7 @@ class NameServer(Service):
 
     def exposed_lookup(self, name: str) -> tuple:
         if name in self.lookup_table.keys():
-            return self.lookup_table['name']
+            return self.lookup_table[name]
         else:
             print(f"Name: {name} does not exist... ")
 
